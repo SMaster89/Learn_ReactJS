2794a13300fc6053cf4c5690d4a770b8eb25ea3e
mentor3 <stivem666@gmail.com>
2020-06-13 00:18:42 +0300

    commit3

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Nav.css';
 
 const Nav = () => {
     return (
         <nav className="nav">
             <ul>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Messages</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Music</a></li>
-                <li><a href="#">Setting</a></li>
+                <li className="item" ><a href="#">Profile</a></li>
+                <li className="item" ><a href="#">Messages</a></li>
+                <li className="item" ><a href="#">News</a></li>
+                <li className="item" ><a href="#">Music</a></li>
+                <li className="item" ><a href="#">Setting</a></li>
             </ul>
         </nav>
     );
