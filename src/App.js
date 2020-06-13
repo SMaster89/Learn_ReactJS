76249d24d816d6c64384cc8085a004b98a2fa320
mentor3 <stivem666@gmail.com>
2020-06-13 19:41:13 +0300

    commit Lesson19

--- src/App.js
+++ src/App.js
@@ -4,21 +4,32 @@ import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+
+
+
 
 
 // function App() {
   const App = (props) => {
-  return (  
-   <div className="app-wrapper">
-     <Header />
-     <Nav />
-
-     <div className="app-wrapper-content">
-         {/*<Profile />*/}
-         <Dialogs />
-     </div>
-    </div>
+  return (
+      <BrowserRouter>
+            <div className="app-wrapper">
+                 <Header />
+                 <Nav />
+                 <div className="app-wrapper-content">
+                     <Route path='/dialogs' component={Dialogs} />
+                     <Route path='/profile' component={Profile} />
+                     <Route path='/News' component={News} />
+                     <Route path='/Music' component={Music} />
+                     <Route path='/Settings' component={Settings} />
+                 </div>
+            </div>
+        </BrowserRouter>
   );
 }
-   
+
 export default App;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,11 +5,11 @@ const Nav = () => {
     return (
         <nav className={s.nav}>
             <ul>
-                <li className={s.item}><a href="#">Profile</a></li>
-                <li className={`${s.item} ${s.active}`}><a href="#">Messages</a></li>
-                <li className={s.item}><a href="#">News</a></li>
-                <li className={s.item}><a href="#">Music</a></li>
-                <li className={s.item}><a href="#">Setting</a></li>
+                <li className={s.item}><a href="/profile">Profile</a></li>
+                <li className={`${s.item} ${s.active}`}><a href="/dialogs">Messages</a></li>
+                <li className={s.item}><a href="/news">News</a></li>
+                <li className={s.item}><a href="/music">Music</a></li>
+                <li className={s.item}><a href="/settings">Settings</a></li>
             </ul>
         </nav>
     );
