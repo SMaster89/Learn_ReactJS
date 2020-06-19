c8e3f7055a7b1f5489ddc5b2debb43821485de9c
mentor3 <stivem666@gmail.com>
2020-06-19 22:46:15 +0300

    commit Lesson36

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,68 +1,4 @@
-// let posts = [
-//     {
-//         id: 1,
-//         likeCount: 15,
-//         message: "Hi, how are you"
-//     },
-//     {
-//         id: 2,
-//         likeCount: 23,
-//         message: "It's my first post"
-//     },
-//     {
-//         id: 3,
-//         likeCount: 9,
-//         message: "Blablabla"
-//     },
-//     {
-//         id: 4,
-//         likeCount: 1,
-//         message: "Dada"
-//     }
-// ]
-
-// let dialogs = [
-//     {
-//         id: 1,
-//         name: "Dimych"
-//     },
-//     {
-//         id: 2,
-//         name: "Andrey"
-//     },
-//     {
-//         id: 3,
-//         name: "Sveta"
-//     },
-//     {
-//         id: 4,
-//         name: "Sasha"
-//     },
-//     {
-//         id: 5,
-//         name: "Viktor"
-//     },
-//     {
-//         id: 6,
-//         name: "Valera"
-//     },
-// ]
-
-// let messages = [
-//     {
-//         id: 1,
-//         message: "Hi"
-//     },
-//     {
-//         id: 2,
-//         message: "How are you?"
-//     },
-//     {
-//         id: 3,
-//         message: "Yo"
-//     },
-//
-// ]
+ 
 
 let rerenderEntireTree = () => {
 
@@ -200,5 +136,6 @@ export const updateNewPostText =(newText) => {
 export const subscribe = (observer) => {
     rerenderEntireTree = observer; //observe- наблюдатель
 }
+
 
 export default state;
