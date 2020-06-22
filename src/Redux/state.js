3d17d50b1499e0510bab18500ed6c81b0276dcec
mentor3 <stivem666@gmail.com>
2020-06-22 20:33:23 +0300

    commit Lesson37

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,141 +1,139 @@
- 
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {
+                    id: 1,
+                    likeCount: 15,
+                    message: "Hi, how are you"
+                },
+                {
+                    id: 2,
+                    likeCount: 23,
+                    message: "It's my first post"
+                },
+                {
+                    id: 3,
+                    likeCount: 9,
+                    message: "Blablabla"
+                },
+                {
+                    id: 4,
+                    likeCount: 1,
+                    message: "Dada"
+                }
+            ],
+            newPostText: 'It-Kamasutra'
+        },
+        dialogsPage: {
+            dialogs: [
+                {
+                    id: 1,
+                    name: "Dimych",
+                    src: "https://avatars.mitosa.net/ring/sm070.jpg"
+                },
+                {
+                    id: 2,
+                    name: "Andrey",
+                    src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-D4-icon.png"
+                },
+                {
+                    id: 3,
+                    name: "Sveta",
+                    src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-D1-icon.png"
+                },
+                {
+                    id: 4,
+                    name: "Sasha",
+                    src: "https://www.freeiconspng.com/uploads/face-avatar-png-14.png"
+                },
+                {
+                    id: 5,
+                    name: "Viktor",
+                    src: "https://www.iconninja.com/files/785/42/357/avatar-face-icon.png"
+                },
+                {
+                    id: 6,
+                    name: "Valera",
+                    src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-H2-icon.png"
+                },
+            ],
+            messages: [
+                {
+                    id: 1,
+                    message: "Hi"
+                },
+                {
+                    id: 2,
+                    message: "How are you?"
+                },
+                {
+                    id: 3,
+                    message: "Yo"
+                },
 
-let rerenderEntireTree = () => {
-
-}
-
-let state = {
-    profilePage: {
-        posts: [
-            {
-                id: 1,
-                likeCount: 15,
-                message: "Hi, how are you"
-            },
-            {
-                id: 2,
-                likeCount: 23,
-                message: "It's my first post"
-            },
-            {
-                id: 3,
-                likeCount: 9,
-                message: "Blablabla"
-            },
-            {
-                id: 4,
-                likeCount: 1,
-                message: "Dada"
-            }
-        ],
-        newPostText: 'It-Kamasutra'
+            ],
+        },
+        friendsPage: {
+            friends: [
+                {
+                    id: 1,
+                    name: "Dimych",
+                    src: "https://avatars.mitosa.net/ring/sm070.jpg"
+                },
+                {
+                    id: 2,
+                    name: "Andrey",
+                    src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-D4-icon.png"
+                },
+                {
+                    id: 3,
+                    name: "Sveta",
+                    src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-D1-icon.png"
+                },
+                {
+                    id: 4,
+                    name: "Sasha",
+                    src: "https://www.freeiconspng.com/uploads/face-avatar-png-14.png"
+                },
+                {
+                    id: 5,
+                    name: "Viktor",
+                    src: "https://www.iconninja.com/files/785/42/357/avatar-face-icon.png"
+                },
+                {
+                    id: 6,
+                    name: "Valera",
+                    src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-H2-icon.png"
+                },
+            ]
+        },
     },
-    dialogsPage: {
-        dialogs: [
-            {
-                id: 1,
-                name: "Dimych",
-                src: "https://avatars.mitosa.net/ring/sm070.jpg"
-            },
-            {
-                id: 2,
-                name: "Andrey",
-                src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-D4-icon.png"
-            },
-            {
-                id: 3,
-                name: "Sveta",
-                src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-D1-icon.png"
-            },
-            {
-                id: 4,
-                name: "Sasha",
-                src: "https://www.freeiconspng.com/uploads/face-avatar-png-14.png"
-            },
-            {
-                id: 5,
-                name: "Viktor",
-                src: "https://www.iconninja.com/files/785/42/357/avatar-face-icon.png"
-            },
-            {
-                id: 6,
-                name: "Valera",
-                src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-H2-icon.png"
-            },
-        ],
-        messages: [
-            {
-                id: 1,
-                message: "Hi"
-            },
-            {
-                id: 2,
-                message: "How are you?"
-            },
-            {
-                id: 3,
-                message: "Yo"
-            },
-
-        ],
+    getState() {
+        return this._state;
     },
-    friendsPage: {
-        friends: [
-            {
-                id: 1,
-                name: "Dimych",
-                src: "https://avatars.mitosa.net/ring/sm070.jpg"
-            },
-            {
-                id: 2,
-                name: "Andrey",
-                src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-D4-icon.png"
-            },
-            {
-                id: 3,
-                name: "Sveta",
-                src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-D1-icon.png"
-            },
-            {
-                id: 4,
-                name: "Sasha",
-                src: "https://www.freeiconspng.com/uploads/face-avatar-png-14.png"
-            },
-            {
-                id: 5,
-                name: "Viktor",
-                src: "https://www.iconninja.com/files/785/42/357/avatar-face-icon.png"
-            },
-            {
-                id: 6,
-                name: "Valera",
-                src: "https://icons.iconarchive.com/icons/hopstarter/face-avatars/256/Male-Face-H2-icon.png"
-            },
-        ]
+    _callSubscriber() {
+        console.log("State changed");
     },
-}
-
-window.state = state;
-
-export const addPost = () => {
-    let newPost = {
+    addPost() {
+        let newPost = {
             id: 5,
-            message: state.profilePage.newPostText,
+            message: this._state.profilePage.newPostText,
             likeCount: 0
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer; //observe- наблюдатель
     }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
 }
 
-export const updateNewPostText =(newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
+window.store = store;
 
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer; //observe- наблюдатель
-}
-
-
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import state, {subscribe} from "./Redux/state";
+import store from "./Redux/state";
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import {addPost, updateNewPostText} from "./Redux/state";
 
  let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+                <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)} />
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
- subscribe(rerenderEntireTree);
+ store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
